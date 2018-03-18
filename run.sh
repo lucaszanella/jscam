4f7685f2a9cca43195b0da8695f599b1343f4dfd
Lucas Zanella <me@lucaszanella.com>
2018-03-18 14:21:46 -0300

    react development initiated

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker-compose run --rm -u $( id -u $USER ):$( id -g $USER )  -v $PWD/src/home:/home/$USER jscam
+docker-compose run --rm -u $( id -u $USER ):$( id -g $USER ) -v $PWD/src/home:/home/$USER jscam
