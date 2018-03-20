3f77d06aa6616d9803b8400888774297408c5a7b
Lucas Zanella <me@lucaszanella.com>
2018-03-20 01:56:09 -0300

    finally worked

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker-compose run --rm -u $( id -u $USER ):$( id -g $USER ) -v $PWD/src/home:/home/$USER -v $HOME/Android/Sdk:/usr/local/android-sdk-linux dev
+docker-compose run --rm -u $( id -u $USER ):$( id -g $USER ) -v $PWD/src/home:/home/$USER -v $HOME/Android/Sdk:/home/lz/Android/Sdk dev
