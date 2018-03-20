8c18ae38f2a40ada80081eed35e997c72739516f
Lucas Zanella <me@lucaszanella.com>
2018-03-19 23:09:28 -0300

    latest commit before ejection

--- src/jscam/App.js
+++ src/jscam/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 //const crypto = require('crypto');
-
 export default class App extends React.Component {
   render() {
     return (
