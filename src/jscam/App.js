17e9ec1e4be3185539179e8263a35e3f2321a67b
Lucas Zanella <me@lucaszanella.com>
2018-04-19 23:04:42 -0300

    adding JNI interface

--- src/jscam/App.js
+++ src/jscam/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import JRTP from './JRTPLIB';
+
+JRTP.show('Awesome');
 //const http = require('http');
 //const cam = require('onvif').Cam;
 
---
+++ src/jscam/android/app/src/main/java/com/JRTPLIB/JRTPLIBModule.java
@@ -0,0 +1,32 @@
+package com.facebook.react.modules.toast;
+
+import android.widget.Toast;
+
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class JRTPModule extends ReactContextBaseJavaModule {
+
+  private static final String DURATION_SHORT_KEY = "SHORT";
+  private static final String DURATION_LONG_KEY = "LONG";
+
+  public JRTPModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  public String getName() {
+    return "JRTPModule";
+  }
+  
+  @ReactMethod
+  public void show(String message, int duration) {
+    Toast.makeText(getReactApplicationContext(), message, duration).show();
+  }
+}
